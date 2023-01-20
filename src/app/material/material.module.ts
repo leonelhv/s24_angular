57b3436db8c1a9f33ed11c5f3eec5c630ae5c570
Leonel Huachaca <leonel.huachacav@gmail.com>
2023-01-20 10:51:12 -0500

    sort import material

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -3,30 +3,30 @@ import { LayoutModule } from '@angular/cdk/layout';
 
 //Modulos de Material
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
 import { MatGridListModule } from '@angular/material/grid-list';
-import { MatCardModule } from '@angular/material/card';
-import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 const materialModules = [
   LayoutModule,
-  MatInputModule,
-  MatFormFieldModule,
-  MatDialogModule,
   MatButtonModule,
-  MatGridListModule,
   MatCardModule,
-  MatMenuModule,
+  MatDialogModule,
+  MatFormFieldModule,
+  MatGridListModule,
   MatIconModule,
-  MatToolbarModule,
-  MatSidenavModule,
+  MatInputModule,
   MatListModule,
+  MatMenuModule,
+  MatSidenavModule,
+  MatToolbarModule,
 ];
 
 @NgModule({
